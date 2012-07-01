84db8e1182a5a53ec1066a8b48ba1b0f56f42643
Balthazar Rouberol <rouberol.b@gmail.com>
2012-07-01 08:38:10 +0200

    typo
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- py/rest_requests.py
+++ py/rest_requests.py
@@ -2,6 +2,6 @@ import requests, json
 
 github_url = "https://api.github.com/user/repos"
 data = json.dumps({'name':'test', 'description':'some test repo'}) 
-r = request.post(github_url, data, auth=('BaltoRouberol', '*****'))
+r = requests.post(github_url, data, auth=('BaltoRouberol', '*****'))
 
 print r.json
